eff6541fec3f396043c5c6176e1d1986b8d92c7c
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-15 19:57:53 +0000

    tasto Recupera Dati in grassetto

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -39,7 +39,7 @@ export default function Dashboard() {
                 <b>Associazione dopo lezioni di selezione o già associato nella stagione precedente:</b>
               </p>
               <ol className="list-decimal list-inside text-sm text-muted-foreground space-y-2 mt-2">
-                <li>Nella scheda associati clicca sul tasto Recupera Dati.</li>
+                <li>Nella scheda associati clicca sul tasto <b>Recupera Dati</b>.</li>
                 <li>Invia la domanda di associazione.</li>
                 <li>Effettua il pagamento del contributo associativo.</li>
               </ol>
