2f593552076ba78879d8ccbf7ee06cafd2ad8cdd
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-15 18:36:40 +0000

    aggiungi al testo nella card associati: Siamo una associazione senza sco

--- src/components/associate-card.tsx
+++ src/components/associate-card.tsx
@@ -19,6 +19,8 @@ export function AssociateCard() {
             <CardContent className="flex-grow">
                 <p className="text-sm text-muted-foreground">
                     Far parte della nostra associazione no profit non significa semplicemente iscriversi a un corso di Arti Marziali. Significa intraprendere un percorso di crescita condiviso, dove l'allenamento fisico è solo una parte di un'esperienza molto più ricca e profonda.
+                    <br /><br />
+                    Siamo una associazione senza scopo di lucro, nata dalla profonda passione per le arti marziali e dalla volontà di condividerne i valori autentici. Ogni aspetto della nostra gestione è guidato da principi di trasparenza e dedizione, tutti i ricavi derivanti dai contributi associativi vengono interamente reinvestiti in Didattica, Formazione, Aggiornamento e progetti di utilità sociale.
                 </p>
             </CardContent>
             <CardFooter className="flex justify-end">
