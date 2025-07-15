0f01a734294183800b44054ad179a0e4f751024c
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-15 20:32:37 +0000

    in prenota stage ed esami crea un riquadro con titolo stage

--- src/components/event-booking.tsx
+++ src/components/event-booking.tsx
@@ -7,18 +7,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "./ui/button"
 
 export function EventBooking() {
   return (
-    <Card>
+    <Card className="h-full flex flex-col">
       <CardHeader>
         <CardTitle>Prenota Stage ed Esami</CardTitle>
         <CardDescription>
           Riserva il tuo posto nei prossimi eventi speciali.
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        {/* Event items will be displayed here in the future */}
+      <CardContent className="flex-grow">
+        <div className="rounded-lg border p-4">
+            <h3 className="font-semibold text-lg mb-2">Stage</h3>
+            {/* Future stage content can go here */}
+        </div>
       </CardContent>
     </Card>
   )
