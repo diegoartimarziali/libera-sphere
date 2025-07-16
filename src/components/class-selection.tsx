cfc563ce7480263bfa8d5b32c4870c64f3570c4e
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-16 13:29:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input id="birthpl

--- src/components/class-selection.tsx
+++ src/components/class-selection.tsx
@@ -171,7 +171,7 @@ export function ClassSelection() {
                             <Label htmlFor="name">Nome e Cognome</Label>
                             <Input id="name" placeholder="Mario Rossi" required />
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                         <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-4">
                              <div className="space-y-2">
                                 <Label htmlFor="birthplace">nato/a a:</Label>
                                 <Input id="birthplace" type="text" placeholder="Roma" required />
@@ -199,7 +199,6 @@ export function ClassSelection() {
                                     </Select>
                                 </div>
                             </div>
-                           
                         </div>
                     </div>
                      <div className="space-y-2">
