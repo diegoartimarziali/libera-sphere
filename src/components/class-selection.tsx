24d50f5777251f9e1ea58557ed4ed3bf11e97f97
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-21 05:21:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDescription>

--- src/components/class-selection.tsx
+++ src/components/class-selection.tsx
@@ -442,7 +442,7 @@ export function ClassSelection({ setLessonSelected, initialStep = 1 }: { setLess
              <Card>
                 <CardHeader>
                     <CardTitle className="bg-primary text-primary-foreground p-6 -mt-6 -mx-6 rounded-t-lg mb-6">Lezioni Selezione</CardTitle>
-                    <CardDescription>
+                    <CardDescription className="text-foreground">
                        Tre incontri per capire e farti capire più un Bonus di inizio percorso di 5 lezioni gratuite. Per garantirti la migliore esperienza possibile e un percorso di crescita personalizzato, abbiamo strutturato una modalità d’ingresso che ti permetterà di farti conoscere e di scoprire il mondo delle arti marziali. Le lezioni di selezione sono un passaggio fondamentale e obbligatorio per chiunque desideri unirsi alla nostra comunità, indipendentemente dall'età e dal livello di esperienza. Ti comunicheremo telefonicamente la data della prima lezione.
                     </CardDescription>
                     <div className="pt-4">
