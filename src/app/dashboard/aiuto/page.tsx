51b5d36703b1ec3fefd0ab46702c6b332b2a2b75
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-22 10:49:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div>
    
    with the te

--- src/app/dashboard/aiuto/page.tsx
+++ src/app/dashboard/aiuto/page.tsx
@@ -8,33 +8,8 @@ export default function AiutoPage() {
                 <CardTitle>Aiuto</CardTitle>
             </CardHeader>
             <CardContent>
-            <div className="grid md:grid-cols-2 gap-8">
-                <div>
-                <p className="text-base text-muted-foreground">
-                    <b>Associazione dopo il Passaporto Selezioni o già associato nella stagione precedente:</b>
-                </p>
-                <ol className="list-decimal list-inside text-base text-muted-foreground space-y-2 mt-2">
-                    <li>Controlla che il tuo certificato medico sia caricato e in corso di validità.</li>
-                    <li>Clicca sul tasto <b>"Recupera Dati"</b>.</li>
-                    <li>Clicca sul tasto <b>"Fai Domanda di Associazione"</b>.</li>
-                    <li>Effettua il pagamento del contributo associativo con il metodo da te scelto.</li>
-                </ol>
-                <Separator className="my-4" />
-                <p className="text-base text-muted-foreground">
-                    <b>Nuova associazione o dati non presenti:</b>
-                </p>
-                <ol className="list-decimal list-inside text-base text-muted-foreground space-y-2 mt-2">
-                    <li>Prendi visione e accetta i regolamenti nella scheda <b>"Accettazione Regolamenti e Privacy"</b>.</li>
-                    <li>Carica il tuo certificato medico nella sezione <b>"Certificato Medico"</b>.</li>
-                    <li>Inserisci i tuoi dati e quelli di un genitore se sei minorenne.</li>
-                    <li>Clicca sul tasto <b>"Fai Domanda di Associazione"</b>.</li>
-                    <li>Effettua il pagamento del contributo associativo con il metodo da te scelto.</li>
-                </ol>
-                </div>
-            </div>
+            
             </CardContent>
         </Card>
     )
 }
-
-    
