1509efb3bea862a81ff6625a7c84de353759416c
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-22 10:48:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div>
    
    with the te

--- src/app/dashboard/aiuto/page.tsx
+++ src/app/dashboard/aiuto/page.tsx
@@ -11,19 +11,6 @@ export default function AiutoPage() {
             <div className="grid md:grid-cols-2 gap-8">
                 <div>
                 <p className="text-base text-muted-foreground">
-                    <b>Passaporto Selezioni, segui questi passaggi:</b>
-                </p>
-                <ol className="list-decimal list-inside text-base text-muted-foreground space-y-2 mt-2">
-                    <li>Prendi visione e accetta i regolamenti nella scheda <b>"Accettazione Regolamenti e Privacy"</b>.</li>
-                    <li>Iscriviti al Passaporto Selezioni tramite la scheda <b>"Passaporto Selezioni"</b>.</li>
-                    <li>Carica il tuo certificato medico nella sezione <b>"Certificato Medico"</b>.</li>
-                    <li>Inserisci i tuoi dati e quelli di un genitore se sei minorenne.</li>
-                    <li>Verrai contattato telefonicamente per scegliere la tua prima lezione.</li>
-                    <li>Effettua il pagamento del contributo di 30€ alla prima lezione.</li>
-                </ol>
-                </div>
-                <div>
-                <p className="text-base text-muted-foreground">
                     <b>Associazione dopo il Passaporto Selezioni o già associato nella stagione precedente:</b>
                 </p>
                 <ol className="list-decimal list-inside text-base text-muted-foreground space-y-2 mt-2">
