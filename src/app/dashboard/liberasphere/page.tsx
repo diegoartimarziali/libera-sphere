ab9ef297eed0d58cabca5e31aa83925190cf89f4
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-22 11:39:05 +0000

    procedi

---
+++ src/app/dashboard/liberasphere/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function LiberaSpherePage() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>LiberaSphere</CardTitle>
+            </CardHeader>
+            <CardContent>
+                {/* Content will be added later */}
+            </CardContent>
+        </Card>
+    )
+}
