185c651ac62014498db96b9814570ee575ef2ff6
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-22 10:54:41 +0000

    aggiungi un testo sotto al nome della scheda: "Se riscontri problemi ho

--- src/app/dashboard/aiuto/page.tsx
+++ src/app/dashboard/aiuto/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 
 export default function AiutoPage() {
@@ -6,9 +6,13 @@ export default function AiutoPage() {
         <Card>
             <CardHeader>
                 <CardTitle>Aiuto</CardTitle>
+                <CardDescription>Se riscontri problemi o hai bisogno di un aiuto per utilizzare l'app:</CardDescription>
             </CardHeader>
             <CardContent>
-            
+                <ol className="list-decimal list-inside space-y-2 text-muted-foreground">
+                    <li>Chiama il numero: <b className="text-foreground">378-0825492</b> (dalle ore 9,00 alle ore 19,00)</li>
+                    <li>Manda un messaggio su Whatsapp: <b className="text-foreground">378-0621692</b></li>
+                </ol>
             </CardContent>
         </Card>
     )
