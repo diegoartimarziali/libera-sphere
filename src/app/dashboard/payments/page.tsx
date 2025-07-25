d53851268c51311b0d35559117dbcf0ed1be21e4
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-25 09:53:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <NewPaymentForm>

--- src/app/dashboard/payments/page.tsx
+++ src/app/dashboard/payments/page.tsx
@@ -1,12 +1,10 @@
 
-import { NewPaymentForm } from "@/components/new-payment-form";
 import { PaymentHistory } from "@/components/payment-history";
 
 export default function PaymentsPage() {
     return (
         <div className="max-w-4xl mx-auto space-y-6">
             <PaymentHistory />
-            <NewPaymentForm />
         </div>
     );
 }
