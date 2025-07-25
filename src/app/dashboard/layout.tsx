91c711dcb78d6c8454c68fe17340052112fc56b5
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-25 14:01:14 +0000

    non riesco più ad accedere o registrarmi: er responded with a status of

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -44,7 +44,7 @@ const DumbbellIcon = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24"
+    viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
@@ -65,7 +65,7 @@ const KanjiIcon = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24"
+    viewBox="0 0 24 24"
     fill="currentColor"
     {...props}
   >
@@ -433,5 +433,3 @@ export default function DashboardLayout({
     </div>
   )
 }
-
-    
