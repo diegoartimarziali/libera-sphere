c80bb18f672e88b5c66b540a2b3a0cd43be10c39
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-07-30 09:51:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDescription>

--- src/app/dashboard/liberasphere/page.tsx
+++ src/app/dashboard/liberasphere/page.tsx
@@ -164,7 +164,7 @@ export default function LiberaSpherePage() {
         <CardHeader>
           <CardTitle className="text-2xl">Benvenuto!</CardTitle>
           <CardDescription>
-            Aiutaci a capire il tuo percorso. Sei già stato dei nostri in passato?
+            Sei già stato Socio di Libera Energia Arti Marziali ?
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-6">
