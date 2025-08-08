c787109851dec7dbfcc535cf000e2c2974bca25e
Diego Carcassi <diego.carcassi@artimarzialivalledaosta.com>
2025-08-08 21:43:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -587,7 +587,7 @@ export default function AdminCalendarPage() {
                                 </SelectTrigger>
                                 <SelectContent>
                                     {gyms.map(gym => (
-                                        <SelectItem key={gym.id} value={gym.id}>{gym.name}</SelectItem>
+                                        <SelectItem key={gym.id} value={gym.id}>{gym.id} - {gym.name}</SelectItem>
                                     ))}
                                 </SelectContent>
                             </Select>
@@ -788,6 +788,8 @@ export default function AdminCalendarPage() {
 
     
 
+
+    
 
     
 
