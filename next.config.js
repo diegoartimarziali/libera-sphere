6247f373bc675ba2161bff9c52e3d0ebbedf8e27
diegoartimarziali <diego.carcassi@artimarzialivalledaosta.com>
2025-09-09 19:25:38 +0200

    RIPETO COMMIT DEL 08/09/2025

---
+++ next.config.js
@@ -0,0 +1,35 @@
+// Converted from next.config.ts to next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['@radix-ui/react-icons'],
+  },
+};
+
+module.exports = nextConfig;
